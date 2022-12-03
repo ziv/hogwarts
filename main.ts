58fcb5e8c834b4945bdd71c2d620938e0294f1e2
Ziv <ziv.perry@zoominfo.com>
2022-12-03 14:55:48 +0200

    fix path

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.167.0/http/server";
+import { serve } from "https://deno.land/std@0.167.0/http/server.ts";
 
 
 serve((req: Request) => new Response("Hello World 1"));
